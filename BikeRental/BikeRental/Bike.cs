159009b908c37a4e9a1e3ccd5c6f9402a2201d98
mgwozdz <mgwozdz@gyfsi.com.ar>
2018-01-03 10:06:23 -0300

    Add business classes.

---
+++ BikeRental/BikeRental/Bike.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeRental
+{
+    public class Bike
+    {
+        private string BikeType { set; get; }
+
+        public int GetPrice()
+        {
+            switch (BikeType)
+            {
+                case "H":
+                    return 5;
+                case "D":
+                    return 20;
+                case "W":
+                    return 60;
+                default:
+                    return 0;
+            }
+        }
+
+        public void SetTypeBike(string typeByke)
+        {
+            this.BikeType = typeByke;
+        }
+    }
+}
---
+++ BikeRental/BikeRental/Promotion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeRental
+{
+    public class Promotion
+    {
+        public Promotion()
+        {
+            Bikes = new List<Bike>();
+        }
+        private List<Bike> Bikes { set; get; }
+
+        public void AddBike(Bike bike)
+        {
+            Bikes.Add(bike);
+        }
+
+        public int GetPrice()
+        {
+            int result = 0;
+            foreach (Bike bike in Bikes)
+            {
+                result += bike.GetPrice();
+            }
+            return result;
+        }
+    }
+}
